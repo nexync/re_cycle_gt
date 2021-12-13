090ad5d3b7d7389ca4d6e8c8b3f89139bf1dec0b
Rami Sbahi <sbahirami@gmail.com>
2021-12-12 22:59:14 -0500

    minor bug fix in t2g with numn entities

--- t2g.py
+++ t2g.py
@@ -182,8 +182,8 @@ class T2GModel():
 				"relations": [],
 				"entities": batch[b]['entities']
 			}
-			for i in range(0, ne):
-				for j in range(i+1, ne):
+			for i in range(0, len(temp['entities'])):
+				for j in range(i+1, len(temp['entities'])):
 					temp['relations'].append([temp['entities'][i], self.vocab.relations.wordlist[preds[b, i, j]], temp['entities'][j]])
 			output.append(temp)
 		return output
